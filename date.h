fb569af2353178e2744cb2909908024e53a90a70
22XNB1 <22XNB1@queensu.ca>
2025-09-23 11:31:08 -0400

    Added Book class and Date class

---
+++ date.h
@@ -0,0 +1,28 @@
+#ifndef DATE_H
+#define DATE_H
+
+class Date {
+private:
+    int Year;
+    int Month;
+    int Day;
+
+public:
+    // Constructor
+    Date(int year, int month, int day);
+
+    // Getters
+    int getYear() const;
+    int getMonth() const;
+    int getDay() const;
+
+    // Setters
+    void setYear(int year);
+    void setMonth(int month);
+    void setDay(int day);
+
+    // Comparison
+    bool isBefore(const Date& date) const;
+};
+
+#endif // DATE_H
