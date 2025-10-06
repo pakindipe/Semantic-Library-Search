9e1ad3ec280981294602760842fc2090851b4858
dkrayacich <david.krayacich@gmail.com>
2025-10-06 09:57:09 -0400

    implemented FAISS type solution

--- python_interface.py
+++ python_interface.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
+from sklearn.preprocessing import normalize
 import sys
 titles = ["Journeys Beyond the Horizon",
           "The Edge of the Map",
@@ -12,39 +13,24 @@ titles = ["Journeys Beyond the Horizon",
           "The Nature of Knowing",
           "Reflections on Reality",
           "The Infinite Within"]
-embs = None
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+embs = model.encode(titles, batch_size=64, convert_to_numpy=True)
+embs = normalize(embs)
 
-def normalize(vec):
-    return vec / (np.linalg.norm(vec) + 1e-12)
+d = 384
 
-def add_book(title:str):
-    global embs
-    titles.append(title)
-    vec = model.encode(title, convert_to_numpy=True).astype("float32")
-    vec = normalize(vec)
-    embs = vec if embs is None else np.vstack([embs, vec])
+index = faiss.IndexFlatIP(d)
+index.add(embs)
 
 def query(query:str):
-    query_vec = model.encode(query, convert_to_numpy=True).astype("float32")
+    query_vec = model.encode([query], convert_to_numpy=True).astype("float32")
     query_vec = normalize(query_vec)
-    scores = embs @ query_vec
-    idx = np.argsort(-scores)
+    D, I = index.search(query_vec, 5)
     ordered_titles = []
-    for i in range(0, 5):
-        ordered_titles.append(titles[idx[i]])
+    for ind in I[0]:
+        ordered_titles.append(titles[ind])
     return ordered_titles
 
-add_book("Journeys Beyond the Horizon")
-add_book("The Edge of the Map")
-add_book("Explorers of the Unknown")
-add_book("Into the Farthest Lands")
-add_book("The Path of Discovery")
-add_book("The Depths of Thought")
-add_book("Horizons of the Mind")
-add_book("The Nature of Knowing")
-add_book("Reflections on Reality")
-add_book("The Infinite Within")
 
 if len(sys.argv) > 1:
     result = query(sys.argv[1])
