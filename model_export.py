1ae23de1816bcbd776970eea226e097f12daba3e
dkrayacich <david.krayacich@gmail.com>
2025-10-11 10:39:13 -0400

    created tf model

--- model_export.py
+++ model_export.py
@@ -1,2 +1,29 @@
 import tensorflow as tf
 import tensorflow_hub as hub
+
+ENCODER_URL = "https://tfhub.dev/google/universal-sentence-encoder/4"
+
+embed_layer = hub.KerasLayer(ENCODER_URL, trainable=False)
+
+class StringEmbedder(tf.Module):
+    def __init__(self, encoder):
+        super().__init__()
+        self.encoder = encoder
+
+    @tf.function(input_signature=[tf.TensorSpec(shape=[None], dtype=tf.string, name="text")])
+    def embed(self, text):
+        vecs = self.encode(text)
+        normed = tf.math.l2_normalize(vecs, axis=1)
+        return {"embedding": normed}
+    
+
+def export(export_dir="export/string_embedder/1"):
+    module = StringEmbedder(embed_layer)
+    tf.saved_model.save(
+        module,
+        export_dir,
+        signatures={"serving_default": module.embed}
+    )
+
+if __name__ == "__main__":
+    export()
