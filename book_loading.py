c5ae340b28cf569ebd9d3c940ba497a12e440a61
dkrayacich <david.krayacich@gmail.com>
2025-11-13 11:00:52 -0500

    removed commented out code

--- book_loading.py
+++ book_loading.py
@@ -46,29 +46,3 @@ with _conn() as c:
         c.execute(query, metadata)
 
 faiss.write_index(index, "book_embeddings.faiss")
-
-
-
-
-# titles = ["Journeys Beyond the Horizon",
-#           "The Edge of the Map",
-#           "Explorers of the Unknown",
-#           "Into the Farthest Lands",
-#           "The Path of Discovery",
-#           "The Depths of Thought",
-#           "Horizons of the Mind",
-#           "The Nature of Knowing",
-#           "Reflections on Reality",
-#           "The Infinite Within"]
-# model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-# embs = model.encode(titles, batch_size=64, convert_to_numpy=True)
-# embs = normalize(embs)
-
-# d = 384
-
-# index = faiss.read_index("book_embeddings.faiss")
-# start_id = index.ntotal
-# num_new = embs.shape[0]
-# ids = np.arange(start_id, start_id + num_new)
-# index.add_with_ids(embs, ids)
-# faiss.write_index(index, "book_embeddings.faiss")
